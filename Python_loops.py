85e15757413b8d618e4537de96f1572f450f66ec
josiebeepboop <josette.bitanga@gmail.com>
2022-09-11 12:00:44 -0400

    added deliverable 2 and 3

--- Python_loops.py
+++ Python_loops.py
@@ -9,11 +9,7 @@ counties_dict = {"Arapahoe":422829,"Denver":463353,"Jefferson":432438}
 for county, voters in counties_dict.items():
     print(f'{county} county has {voters:,} registered voters.')
 
-# voting_data = [{"county":"Arapahoe","registered_voters":422829},
-#                 {"county":"Denver","registered_voters":463353},
-#                 {"county":"Jefferson","registered_voters":432438}
-#                 ]
-# county = voting_data.keys()
-# voters = voting_data.values()
-# for county, voters in voting_data.items():
-#     print(f"{voting_data.keys} county.")
+voting_data = {"county":"Arapahoe","registered_voters":422829},
+                {"county":"Denver","registered_voters":463353},
+                {"county":"Jefferson","registered_voters":432438}
+print(f"{voting_data['county']} county has {voting_data['registered_voters']} registered voters.")
